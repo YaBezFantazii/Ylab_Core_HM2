e4c27ffef38e5c1a4f6659840d349b0cedd032ca
Fantazi <kancler-junior@yandex.ru>
2022-09-08 05:38:45 +0300

    Initial commit

---
+++ src/com/company/Task2Test.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task2Test {
+
+    @Test
+    void findSumm() {
+
+        int itog1 = 5;
+        int[] test1 = new int[]{5,6,3,2,5,1,4,9};
+        int itog2 = 442;
+        int[] test2 = new int[]{11,25,33,25,11,54,4,4,54,25};
+        int itog3 = 6;
+        int[] test3 = new int[]{};
+        int itog4 = -7;
+        int[] test4 = new int[]{-5,8,3,-3,-11,3,2,-4};
+
+        assertArrayEquals(new String[]{"3","2"}, ComplexExamples.findSum(test1,itog1));
+        assertArrayEquals(new String[]{"Нет подходящих пар чисел"}, ComplexExamples.findSum(test2,itog2));
+        assertArrayEquals(new String[]{"Нет подходящих пар чисел"}, ComplexExamples.findSum(test3,itog3));
+        assertArrayEquals(new String[]{"-3","-4"}, ComplexExamples.findSum(test4,itog4));
+
+    }
+}
---
+++ src/com/company/Task3Test.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task3Test {
+
+    @Test
+    void fuzzySearch() {
+
+        assertEquals(true,ComplexExamples.fuzzySearch("car", "ca6$$#_rtwheel"));
+        assertEquals(false,ComplexExamples.fuzzySearch(" ", "ca6$$#_rtwheel"));
+        assertEquals(true,ComplexExamples.fuzzySearch(" ", "ca6$$#_ rtwheel"));
+        assertEquals(false,ComplexExamples.fuzzySearch("carew", "ca"));
+        assertEquals(true,ComplexExamples.fuzzySearch("carew", "car_e_w"));
+        assertEquals(false,ComplexExamples.fuzzySearch("", "gfd"));
+        assertEquals(false,ComplexExamples.fuzzySearch("fdsvc##", ""));
+        assertEquals(false,ComplexExamples.fuzzySearch("", ""));
+    }
+}
